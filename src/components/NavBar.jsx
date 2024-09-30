f525a937aaa57ff2190edebb9b6ea32495c7233f
AYTC <aytacalir@gmail.com>
2024-09-30 20:01:31 +0300

    aa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,14 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { red } from '@mui/material/colors';
 
 
 const NavBar = () => {
   return (
     <div>
-        <Box sx={{ flexGrow: 1 }}>
+        <Box color={red} sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
